4d44ca6c8f2440a032f5360c5dfdea5dfc20ee73
rifanabelputra <rifanabelaputra@gmail.com>
2025-11-03 14:03:28 +0700

    Fitur: Menambahkan file koneksi database

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+echo '<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_mahasiswa";
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+if (!$koneksi) {
+    die("Koneksi Gagal: " . mysqli_connect_error());
+}
+
+// echo "Koneksi Berhasil!";
+?>' > koneksi.php
